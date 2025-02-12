f539d191b22f9bf59a134424a6fce672ba4f2e99
Abdulfetah <abdulfetah@gmail.com>
2025-02-12 16:56:05 +0300

    home page back to /pages directory

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('pages.home');
 })->name('home');
 
 Route::get('/blog', function() {
